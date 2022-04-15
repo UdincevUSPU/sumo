03416f97d377d07805ecc74ac7f4a9a9e455e8d1
IvanU <curseangel1@gmail.com>
2022-04-15 12:57:27 +0500

    poisk

--- main.ts
+++ main.ts
@@ -22,6 +22,14 @@ function Право () {
     pins.digitalWritePin(DigitalPin.P16, 0)
     pins.analogWritePin(AnalogPin.P1, лево)
 }
+function Рывок () {
+    pins.analogWritePin(AnalogPin.P0, 900)
+    pins.analogWritePin(AnalogPin.P1, 900)
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+}
 function назад () {
     pins.analogWritePin(AnalogPin.P0, право)
     pins.digitalWritePin(DigitalPin.P13, 0)
@@ -38,11 +46,44 @@ function Лево () {
     pins.digitalWritePin(DigitalPin.P16, 0)
     pins.analogWritePin(AnalogPin.P1, лево)
 }
+let a = 0
 let лево = 0
 let право = 0
-право = 600
-лево = 600
+право = 200
+лево = 200
 basic.forever(function () {
-    Вперед()
-    basic.pause(10000)
+    if (input.buttonIsPressed(Button.A)) {
+        a = 1
+    }
+    if (a) {
+        if (sonar.ping(
+        DigitalPin.P9,
+        DigitalPin.P8,
+        PingUnit.Centimeters
+        ) > 15) {
+            if (pins.analogReadPin(AnalogPin.P2) < 500) {
+                Лево()
+                basic.showLeds(`
+                    . . # . .
+                    . . # . .
+                    . # . # .
+                    . # . # .
+                    # . . . #
+                    `)
+            }
+        } else {
+            if (pins.analogReadPin(AnalogPin.P2) < 500) {
+                Рывок()
+                basic.showLeds(`
+                    # . . . #
+                    . # . # .
+                    . . # . .
+                    . # . # .
+                    # . . . #
+                    `)
+            } else {
+                стоп()
+            }
+        }
+    }
 })
